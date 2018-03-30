78c8423ac3576ed5b48fc935e5279a4def010f69
soren-cloud <soren@lindevang.it>
2018-03-30 16:34:49 +0200

    First Commit

---
+++ ExO-EnableMailboxAuditLogging.ps1
@@ -0,0 +1,8 @@
+﻿$ch_env = Get-ChildItem env:
+
+if (!($ch_env.AUTOMATION_ASSET_ACCOUNTID))
+    {
+    Write "This script is not executed in Azure Automation."
+    }
+
+$ch_env.AUTOMATION_ASSET_ACCOUNTID
