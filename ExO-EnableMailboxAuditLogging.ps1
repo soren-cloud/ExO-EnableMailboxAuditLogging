125ec005143c72ae84538db281566c9a14aa6c4b
Soren Lindevang <31589772+soren-cloud@users.noreply.github.com>
2018-03-30 16:35:42 +0200

    Delete ExO-EnableMailboxAuditLogging.ps1

--- ExO-EnableMailboxAuditLogging.ps1
+++
@@ -1,8 +0,0 @@
-﻿$ch_env = Get-ChildItem env:
-
-if (!($ch_env.AUTOMATION_ASSET_ACCOUNTID))
-    {
-    Write "This script is not executed in Azure Automation."
-    }
-
-$ch_env.AUTOMATION_ASSET_ACCOUNTID
